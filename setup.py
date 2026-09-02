c4b791ddffb73364705e554fa48459b55aa6a066
Kyle Walker <54470811+hiimkir@users.noreply.github.com>
2026-09-02 10:13:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="Telegram Desktop decrypter",
     version="1.2",
     packages=['tdesktop_decrypter'],
-    install_requires=['tgcrypto'],
+    install_requires=['tgcrypto-pyrofork'],
     entry_points={
         'console_scripts': ['tdesktop-decrypter=tdesktop_decrypter:main']
     }
